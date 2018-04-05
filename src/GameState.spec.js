a9a36b9d5f83cec08bf4f2fc04bb370ea854741e
Edwin Tangenberg <Edwin.Tangenberg@poldervalley.com>
2018-04-05 13:29:11 +0200

    Added commented logging and added mpre checking(not all)

--- src/GameState.spec.js
+++ src/GameState.spec.js
@@ -15,7 +15,12 @@ describe('GameState', () => {
         const state =  new GameState(player, 8, 8);
         const pieces = state.getInitialTileStates(0,1);
 
-        console.log('Pieces', pieces);
+        // console.log('Pieces', pieces);
         expect(pieces[0][0].pieces.length).toBe(0);        
+        expect(pieces[1][0].pieces.length).toBe(1);        
+        expect(pieces[2][0].pieces.length).toBe(0);        
+        expect(pieces[0][1].pieces.length).toBe(1);        
+        expect(pieces[1][1].pieces.length).toBe(0);        
+        expect(pieces[2][1].pieces.length).toBe(1);        
     });
 });
